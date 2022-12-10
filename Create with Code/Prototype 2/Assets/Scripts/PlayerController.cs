02cf030be69106e65130ae4971cc623c38a43c79
BrianStormYT <gameboy0429@gmail.com>
2022-12-10 15:36:15 -0800

    Initial Commit
    All Programs work except potentially Program 2 and 3

---
+++ Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float horizontalInput;
+    public float speed = 20.0f;
+    public float xRange = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        //Launch a projectile from the player
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+      }
+      //Keep player in bounds
+      if (transform.position.x < -xRange)
+      {
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+      }
+      if (transform.position.x > xRange)
+      {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+      }
+       horizontalInput = Input.GetAxis("Horizontal");
+       transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
---
+++ Create with Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool gameOver = false;
+    public bool isOnGround = true;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        //playerRb.addForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Ground")){
+            isOnGround = true;
+            dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")){
+            gameOver = true;
+            Debug.Log("Game Over!"); 
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+        
+}
